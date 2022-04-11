c74241631959f0daab31a3aeb4337aee03022a4a
Buzz2d0 <admin@imipy.com>
2022-04-11 18:47:09 +0800

    Add random

---
+++ random/strings.go
@@ -0,0 +1,35 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+)
+
+var (
+	DigitSeeds       = []byte("0123456789")
+	UpperLetterSeeds = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	LowerLetterSeeds = []byte("abcdefghijklmnopqrstuvwxyz")
+	PunctuationSeeds = []byte("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+)
+
+func RandomString(length int) string {
+	return randomString(append(append(DigitSeeds, UpperLetterSeeds...), LowerLetterSeeds...), length)
+}
+
+func RandomDigitString(length int) string {
+	return randomString(DigitSeeds, length)
+}
+
+func RandomStringWithSeeds(length int, seeds []byte) string {
+	return randomString(seeds, length)
+}
+
+func randomString(seeds []byte, length int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	slice := make([]byte, length, length)
+	for i := 0; i < length; i++ {
+		slice[i] = seeds[r.Intn(len(seeds))]
+	}
+	return string(slice)
+}
